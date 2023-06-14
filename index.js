49342a7996cf21ef30c2f28103bd74841ad555a1
Ralf S. Pellenz <ralfsnt@gmail.com>
2023-06-14 17:32:35 -0300

    Exercício 3

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function somar(num1 = 0, num2 = 0) {
   return num1 + num2;
 }
-console.log(somar(10, 23)); 
-
+console.log(somar(10, 23));
 
 //   Exercício 2   //
 
@@ -10,3 +9,18 @@ function dividir(dividendo, divisor) {
   return Math.floor(dividendo / divisor);
 }
 console.log(dividir(11, 5));
+
+//   Exercício 3   //
+
+function subtrair(minuendo, subtraendo) {
+  if (
+    minuendo == null ||
+    minuendo == undefined && subtraendo == null ||
+    subtraendo == undefined
+  ) {
+    console.log("Impossível realizar a operação");
+  } else {
+    return minuendo - subtraendo;
+  }
+}
+console.log(subtrair(9, 8));
