102160764563f71c7476fa1f60b5937d7fa240b0
Ralf S. Pellenz <ralfsnt@gmail.com>
2023-06-14 17:18:08 -0300

    Exercício 2

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function somar(num1, num2) {
+function somar(num1 = 0, num2 = 0) {
   return num1 + num2;
 }
-console.log(somar(10, 23));
+console.log(somar(10, 23)); 
+
+
+//   Exercício 2   //
+
+function dividir(dividendo, divisor) {
+  return Math.floor(dividendo / divisor);
+}
+console.log(dividir(11, 5));
