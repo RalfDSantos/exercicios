7755a10a449b03829864f82208ad1362e2ba1794
Ralf S. Pellenz <ralfsnt@gmail.com>
2023-06-16 03:45:16 -0300

    Exercício 4

--- index.js
+++ index.js
@@ -15,14 +15,35 @@ console.log(dividir(11, 5));
 function subtrair(minuendo, subtraendo) {
   if (
     minuendo === null ||
-    minuendo === undefined && subtraendo === null ||
+    (minuendo === undefined && subtraendo === null) ||
     subtraendo === undefined
   ) {
-    return("Impossível realizar a operação");
+    return "Impossível realizar a operação";
   } else {
     return minuendo - subtraendo;
   }
 }
-console.log(subtrair(9, 8));  //a saída vai ser normal, pois os dois valores foi atribuidos
+console.log(subtrair(9, 8)); //a saída vai ser normal, pois os dois valores foi atribuidos
 
 // console.log(subtrair(5)); //a saída vai dar erro, pois só um valor foi atribuido
+
+//   Exercício 4   //
+
+function multiplicar(multiplicando, multiplicador) {
+  if (typeof multiplicando && typeof multiplicador !== Number) {
+    return "A Operação é efetuada somente com valores do tipo numero";
+  }
+
+  if (
+    (multiplicando && multiplicador == "") ||
+    (multiplicando && multiplicador == null) ||
+    (multiplicando && multiplicador == undefined)
+  ) {
+    return "Impossível realizar a operação";
+  } else {
+    return Math.floor(multiplicando * multiplicador);
+  }
+}
+console.log(multiplicar(5, 5)); // aqui a função vai dar certo
+// console.log(multiplicar(5, "5"));  // aqui a função vai dar errado
+// console.log(multiplicar(5, ""));  // aqui a função vai dar errado
